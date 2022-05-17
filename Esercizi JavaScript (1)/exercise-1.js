3df4b124069bf4c36ecb7ec733b0a761a14a942a
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-05-17 17:06:08 +0200

    add ex1 js

--- Esercizi JavaScript (1)/exercise-1.js
+++ Esercizi JavaScript (1)/exercise-1.js
@@ -1,7 +1,9 @@
 function canPlay() {
-  if (true) {
-    let personName = 'Paul';
-  }
+  
+  let personName = 'Paul';
+  // tolto if poichè let e const sono block scoped, quindi non 
+  // accessibili all'esterno del proprio blocco, in questo caso if
+  
 
   personName += ' plays football'; // Argh! personName is not defined
 
