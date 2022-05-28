d9df59098cf056cf8fb3d46020029a5415e40a56
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-05-28 13:24:40 +0200

    fix ex 23

--- ex (23)/exercise-23.js
+++ ex (23)/exercise-23.js
@@ -1,12 +1,28 @@
 const person = {
   firstName: 'Mario',
   lastName: 'Rossi',
-  age: 25
+  age: 25,
+}
+
+const person2 = {
+  firstName: 'Ludovico',
+  lastName: 'Colucci',
+  age: 21,
 }
 
 // Print values of person using Object.keys
 
-console.log(Object.keys(person)); //ottengo solo le chiavi dell'oggetto
+// console.log(`${Object.keys(person)[0]}: ${Object.values(person)[0]}`);
+// console.log(`${Object.keys(person)[1]}: ${Object.values(person)[1]}`);
+// console.log(`${Object.keys(person)[2]}: ${Object.values(person)[2]}`);
 
-console.log(Object.entries(person)); //ottengo chiavi e relativi valori dell'oggetto
+function infoPerson (item) {
+  console.log(`${Object.keys(item)[0]}: ${Object.values(item)[0]}`);
+  console.log(`${Object.keys(item)[1]}: ${Object.values(item)[1]}`);
+  console.log(`${Object.keys(item)[2]}: ${Object.values(item)[2]}`);
+}
+
+infoPerson(person); //so che la funzione non era necessaria ma volevo provare
+
+
 
