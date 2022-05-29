8bd4d7d6511667152fc836b04ed2d353fecca9ea
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-05-29 16:36:20 +0200

    add ex 34

---
+++ ex (10)/exercise-10.js
@@ -0,0 +1,17 @@
+function sumUntil(maxValue) {
+  let counter = 0;
+  while (counter <= maxValue) {
+    console.log(counter);
+    counter ++;
+    // return counter
+  }
+}
+
+
+console.log(sumUntil(5));
+// sumUntil(5);
+
+// let maxValue = 10;
+// for (let i = 1; i <= maxValue; i++) {
+//   console.log(i);
+// }
