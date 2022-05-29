5beffefd3043f36f1cec187250a228d7a08f4b30
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-05-29 18:08:07 +0200

    add ex 40

--- ex (40)/exercise-40.js
+++ ex (40)/exercise-40.js
@@ -1,10 +1,45 @@
 class Person {
-  // ...
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  fullName(){
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  // get firstName(){
+  //   return this.firstName;
+  // }
+
+  // get lastName(){
+  //   return this.lastName;
+  // }
+
+  //  get age(){
+  //   return this.age;
+  // }
+
+  //  set firstName(value){
+  //   this.firstName = value;
+  // }
+
+  // set lastName(value){
+  //   this.lastName = value;
+  // }
+
+  // set age(value){
+  //   this.age = value;
+  // }
+
+  // non capisco il funzionamento di get e set
+
 }
 
 const person = new Person('Mario', 'Rossi', 25);
-console.log(person.fullName);
+console.log(person.fullName());
 
 person.firstName = 'Maria';
 person.lastName = 'Verdi';
-console.log(person.fullName);
+console.log(person.fullName());
