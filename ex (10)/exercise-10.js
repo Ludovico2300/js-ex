74da11889936d9720d5b469dd8519377f512169e
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-06-06 14:53:19 +0200

    add ex 10

--- ex (10)/exercise-10.js
+++ ex (10)/exercise-10.js
@@ -1,17 +1,8 @@
 function sumUntil(maxValue) {
   let counter = 0;
-  while (counter <= maxValue) {
-    console.log(counter);
-    counter ++;
-    // return counter
+  for (let i = 1; i <= maxValue; i++){
+    counter = counter + i;
   }
+  return counter
 }
-
-
 console.log(sumUntil(5));
-// sumUntil(5);
-
-// let maxValue = 10;
-// for (let i = 1; i <= maxValue; i++) {
-//   console.log(i);
-// }
