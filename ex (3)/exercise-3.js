edf6d6a36084df3d2d48ac7be59b8e7b6f716f27
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-06-06 13:30:53 +0200

    add ex 3

--- ex (3)/exercise-3.js
+++ ex (3)/exercise-3.js
@@ -1,8 +1,10 @@
 const students = ['Paul', 'George', 'Lucas'];
 
 function addStudent(student) {
-  // ...
-}
+  students.push(student);
+} //usiamo il metodo push per aggiungere l'elemento in array
+//anche se è una costante, possiamo aggiungere elementi in coda, 
+//perchè non stiamo riassegnando i valori
 
 addStudent('Marco');
 console.log(students);
