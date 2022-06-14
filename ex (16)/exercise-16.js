9419a905fee183758da662cb02dd8c2755097394
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-06-14 14:01:06 +0200

    add ex 16

--- ex (16)/exercise-16.js
+++ ex (16)/exercise-16.js
@@ -5,12 +5,12 @@ function createStore() {
 
   let products = [];
   
-  function addtoStore (obj) {
+  return function addtoStore (obj) {
   products.push(obj);
   console.log(products);
   }
 
-  console.log(addtoStore());
+  // console.log(addtoStore());
 
   
 
@@ -18,12 +18,14 @@ function createStore() {
 
 const redPants = { id: 1, name: 'Red Pants' };
 const whiteHat = { id: 2, name: 'White Hat' };
-const blackSneakers = { id: 3, name: 'Black Sneakers' };
+const blackHat = { id: 3, name: 'Black Hat' };
+const blackSneakers = { id: 4, name: 'Black Sneakers' };
 
 console.log('--- Dress Store ---');
 const dressStore = createStore();
 dressStore(redPants);
 dressStore(whiteHat);
+dressStore(blackHat);
 
 console.log('--- Shoes Store ---');
 const shoesStore = createStore();
